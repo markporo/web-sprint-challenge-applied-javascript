b5215b24b0b4797827cebc338b925cd2f53368d6
markporo <markporo@yahoo.com>
2021-07-17 17:17:43 -0400

    built header component that appends header to webpage

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,32 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+
+  // create elements
+  const headerDiv = document.createElement("div");
+  const spanDate = document.createElement("span");
+  const h1Title = document.createElement("h1");
+  const spanTemp = document.createElement("span");
+
+  // add classes
+  headerDiv.classList.add("header");
+  spanDate.classList.add("date");
+  h1Title.classList.add("title");
+  spanTemp.classList.add("temp");
+
+  // add inner content
+  spanDate.textContent = date;
+  h1Title.textContent = title;
+  spanTemp.textContent = temp;
+
+  // append children/ hierarchy
+  headerDiv.appendChild(spanDate);
+  headerDiv.appendChild(h1Title);
+  headerDiv.appendChild(spanTemp);
+
+  // return header
+
+  return headerDiv;
 }
 
 const headerAppender = (selector) => {
@@ -20,6 +46,64 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+  // access parent element
+  const parent = document.querySelector(selector);
+
+  // title
+  let newsTitle = "The Lambda Herald";
+
+  // Date
+  let date = new Date();
+  let month;
+
+  switch (date.getMonth()) {
+    case 0:
+      month = "January";
+      break;
+    case 1:
+      month = "February";
+      break;
+    case 2:
+      month = "March";
+      break;
+    case 3:
+      month = "April";
+      break;
+    case 4:
+      month = "May";
+      break;
+    case 5:
+      month = "June";
+      break;
+    case 6:
+      month = "July";
+      break;
+    case 7:
+      month = "August";
+      break;
+    case 8:
+      month = "September";
+      break;
+    case 9:
+      month = "October";
+      break;
+    case 10:
+      month = "November";
+      break;
+    case 11:
+      month = "December";
+      break;
+
+    default:
+    // code block
+  }
+  let todaysDate = month + " " + date.getDate() + " , " + date.getFullYear();
+
+  //temp
+  const temperature = "83 degrees";
+
+  // append Header
+  parent.appendChild(Header(newsTitle, todaysDate, temperature));
 }
 
 export { Header, headerAppender }
